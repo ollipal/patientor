bb145f5ef9239ba428bf801813ceee3ed70b63bf
Olli Paloviita <olli.paloviita@gmail.com>
2021-10-07 14:02:27 +0300

    9.20 done

--- src/components/PatientPage.tsx
+++ src/components/PatientPage.tsx
@@ -7,30 +7,47 @@ import { useStateValue } from "../state";
 import { updatePatient } from "../state";
 
 const PatientPage = () => {
-    const {id} = useParams<{id: string}>();
-    const [{ patients }, dispatch] = useStateValue();
+  const {id} = useParams<{id: string}>();
+  const [{ patients }, dispatch] = useStateValue();
 
-    useEffect(() => {    
-        const fetchPatientList = async () => {
-          try {
-            const { data: patientData } = await axios.get<Patient>(
-              `${apiBaseUrl}/patients/${id}`
-            );
-            dispatch(updatePatient(patientData));
-          } catch (e) {
-            console.error(e);
-          }
-        };
-        if (!patients[id]?.ssn) { // only call if required
-          void fetchPatientList();
-        }
-      }, []);
+  useEffect(() => {    
+    const fetchPatientList = async () => {
+      try {
+        const { data: patientData } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch(updatePatient(patientData));
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    if (!patients[id]?.ssn) { // only call if required
+      void fetchPatientList();
+    }
+  }, []);
+
   return (
     <>
-      <h1>{patients[id] && patients[id].name}</h1>
-      <div>{patients[id]?.gender && `gender: ${patients[id].gender}`}</div>
-      <div>{patients[id]?.ssn && `ssn: ${patients[id]?.ssn || "not available"}`}</div>
-      <div>{patients[id]?.occupation && `occupation: ${patients[id].occupation}`}</div>
+      {patients[id] ?
+        <>
+            <h1>{patients[id].name}</h1>
+            <div>{`gender: ${patients[id].gender}`}</div>
+            <div>{`ssn: ${patients[id]?.ssn || ""}`}</div>
+            <div>{`occupation: ${patients[id].occupation}`}</div>
+            <br/>
+            <b>{patients[id].entries?.length > 0 && "entries"}</b>
+            {
+              patients[id].entries?.map(entry => (
+                <div key={entry.id}>
+                  <div>{entry.date}</div>
+                  <i>{entry.description}</i><br/>
+                  {entry.diagnosisCodes && <div>codes: {entry.diagnosisCodes?.join(", ")}</div>}
+                  <br/>
+                </div>
+              ))
+            }
+        </>
+    : <></>}
     </>
   );
 };
